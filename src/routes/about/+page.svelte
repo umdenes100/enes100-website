d6062e6c6c4f16be957ef2471dc742f1913fbf71
Camille Neumann <camillemneumann@gmail.com>
2024-08-30 15:30:03 -0400

    built about and team pages

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,73 @@
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias exercitationem magnam voluptatibus. Beatae, nihil, quis? Architecto at dolorem et, illum inventore ipsa, ipsam laborum maiores natus, nobis officiis recusandae vitae.
+<style>
+    .split_box {
+        display: flex;
+        padding-top: 25px;
+        padding-bottom: 30px;
+    }
+    .box_split_left {
+        width: 50%;
+        padding-right: 60px;
+    }
+    .box_split_right {
+        width: 50%;
+        padding-left: 60px;
+    }
+    .text {
+        padding-top: 35px;
+        padding-bottom: 50px;
+        padding-right: 90px;
+        padding-left: 90px;
+    }
+    h1 {
+        font-size: 50px;
+    }
+    h2 {
+        font-weight: normal;
+        font-size: 30px;
+    }
+    h3 {
+        font-weight: normal;
+        text-align: center;
+        font-size: 30px;
+        color: #ffd200;
+    }
+    p {
+        font-weight: lighter;
+        margin: auto;
+        font-size: 20px;
+        line-height: 40px;
+    }
+    h1 span {
+        color: #ffd200;
+    }
+    h2 span {
+        text-decoration: 2px underline #ffd200;
+        font-weight: bold;
+    }
+    img {
+        outline: 2px solid #989898;
+        outline-offset: 20px;
+    }
+</style>
+
+<div class="text">
+    <h1>Building the next generation of <span>Engineers.</span></h1>
+    <h2>Talking about <span>ENES100</span> yeah in this block of text we talk about the class. For sure much talking about the class. Talking about ENES100 yeah in this block of text we talk about the <span>class</span>. For sure much talking about the class. In this house we really love this class and want to support it <span>monetarily</span>.</h2>
+    <div class="split_box">
+        <div class="box_split_left">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad molestias, provident inventore iure aliquid corrupti eligendi sint officiis, fuga dignissimos magni explicabo consequuntur, cum deleniti qui reprehenderit? Repellendus, provident perspiciatis.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad molestias, provident inventore iure aliquid corrupti eligendi sint officiis, fuga dignissimos magni explicabo consequuntur, cum deleniti qui reprehenderit? Repellendus, provident perspiciatis.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad molestias, provident inventore iure aliquid corrupti eligendi sint officiis, fuga dignissimos magni explicabo consequuntur, cum deleniti qui reprehenderit? Repellendus, provident perspiciatis.
+        </div>
+        <img src="/src/img/gogi.svg" alt="kitty cat" width="50%">
+    </div>
+    <h3>Interlude text... maybe some fun piece of info can go here owo</h3>
+    <div class="split_box">
+        <img src="/src/img/gogi.svg" alt="kitty cat" width="50%">
+        <div class="box_split_right">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad molestias, provident inventore iure aliquid corrupti eligendi sint officiis, fuga dignissimos magni explicabo consequuntur, cum deleniti qui reprehenderit? Repellendus, provident perspiciatis.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad molestias, provident inventore iure aliquid corrupti eligendi sint officiis, fuga dignissimos magni explicabo consequuntur, cum deleniti qui reprehenderit? Repellendus, provident perspiciatis.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad molestias, provident inventore iure aliquid corrupti eligendi sint officiis, fuga dignissimos magni explicabo consequuntur, cum deleniti qui reprehenderit? Repellendus, provident perspiciatis.
+        </div>
+    </div>
+</div>
--- src/routes/about/team/+page.svelte
+++ src/routes/about/team/+page.svelte
@@ -1,3 +1,211 @@
+<style>
+    .split_box {
+        display: flex;
+        padding-top: 25px;
+        padding-bottom: 30px;
+        display: flex;        
+        justify-content: flex-start;
+        flex-wrap: wrap;
+    }
+    .text {
+        padding-top: 35px;
+        padding-bottom: 50px;
+        padding-right: 90px;
+        padding-left: 90px;
+    }
+    h2 {
+        font-weight: normal;
+        font-size: 30px;
+        margin: auto;
+        color: #ffd200;
+        text-decoration: underline 2px white;
+    }
+    h1 {
+        font-weight: normal;
+        font-size: 40px;
+    }
+    .person {
+        width: 15%;
+        padding: 10px;
+        border: 2px solid rgb(152, 152, 152);
+        margin-bottom: 20px;
+        margin-right: 30px;
+        background-color: #0a131a;
+    }
+    .bio {
+        padding: 10px;
+    }
+    img {
+        width: auto;
+        border: 2px solid rgb(152, 152, 152);
+    }
+</style>
+
 <div class="text">
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias exercitationem magnam voluptatibus. Beatae, nihil, quis? Architecto at dolorem et, illum inventore ipsa, ipsam laborum maiores natus, nobis officiis recusandae vitae.
+    <h1>Meet the team behind the best class anyone has ever taken at any University in the world.</h1>
+    <h2>Instructors</h2>
+    <div class="split_box">
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Job Title<br>Section(s): 0101, 0201, 0301<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Job Title<br>Section(s): 0101, 0201, 0301<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Job Title<br>Section(s): 0101, 0201, 0301<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Job Title<br>Section(s): 0101, 0201, 0301<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Job Title<br>Section(s): 0101, 0201, 0301<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Job Title<br>Section(s): 0101, 0201, 0301<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Job Title<br>Section(s): 0101, 0201, 0301<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Job Title<br>Section(s): 0101, 0201, 0301<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Job Title<br>Section(s): 0101, 0201, 0301<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Job Title<br>Section(s): 0101, 0201, 0301<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Job Title<br>Section(s): 0101, 0201, 0301<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Job Title<br>Section(s): 0101, 0201, 0301<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Job Title<br>Section(s): 0101, 0201, 0301<br>email</div>
+        </div>
+    </div>
+    <h2>Undergraduate Teaching Fellows</h2>
+    <div class="split_box">
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>Section: 0101<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>Section: 0101<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>Section: 0101<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>Section: 0101<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>Section: 0101<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>Section: 0101<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>Section: 0101<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>Section: 0101<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>Section: 0101<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>Section: 0101<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>Section: 0101<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>Section: 0101<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>Section: 0101<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>Section: 0101<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>Section: 0101<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>Section: 0101<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>Section: 0101<br>email</div>
+        </div>
+    </div>
+    <h2>Labratory Teaching Fellows</h2>
+    <div class="split_box">
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>email</div>
+        </div>
+        <div class="person">
+            <img src="/src/img/gogi.svg" alt="kitty cat">
+            <div class="bio">Mr professor<br>Major<br>email</div>
+        </div>
+    </div>
 </div>
