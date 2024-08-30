3cb48a8da7994420a16b575b92ff55c465afd84b
Camille Neumann <camillemneumann@gmail.com>
2024-08-30 15:59:27 -0400

    built student showcase page

--- src/routes/about/showcase/+page.svelte
+++ src/routes/about/showcase/+page.svelte
@@ -1,3 +1,54 @@
-<div class="text">
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias exercitationem magnam voluptatibus. Beatae, nihil, quis? Architecto at dolorem et, illum inventore ipsa, ipsam laborum maiores natus, nobis officiis recusandae vitae.
+<style>
+    .blocky {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        width: 80%;
+        margin: auto;
+        margin-top: 80px;
+    }
+    .box {
+        width: 40%;
+        aspect-ratio: 1/1;
+        background-color: white;
+        margin-bottom: 120px;
+        outline: 2px solid white;
+        outline-offset: 30px;
+        color: black;
+    }
+    .otv {
+        width: 40%;
+        aspect-ratio: 1/1;
+        background-color: white;        
+        margin-bottom: 120px;
+        outline: 2px solid white;
+        outline-offset: 30px;
+    }
+</style>
+
+<div class="blocky">
+    <div class="box">
+        <h1>Fire Mission OTV</h1>
+        <h2>blurb about the team</h2>
+        <ul>
+            <li>unique feature 1</li>
+            <li>unique feature 1</li>
+            <li>unique feature 1</li>
+        </ul>
+    </div>
+    <div class="otv">
+        <img src="/src/img/gogi.svg" alt="kitty cat" height=100%>
+    </div>
+    <div class="otv">
+        <img src="/src/img/gogi.svg" alt="kitty cat" height=100%>
+    </div>
+    <div class="box">
+        hello
+    </div>
+    <div class="box">
+        hello
+    </div>
+    <div class="otv">
+        <img src="/src/img/gogi.svg" alt="kitty cat" height=100%>
+    </div>
 </div>
