ece4eea8ebded94642ae326c6590ed427073d36e
Forrest Milner <forrestmilner+github@gmail.com>
2024-09-25 11:37:45 -0400

    trailingSlash always

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import {vitePreprocess} from '@sveltejs/kit/vite';
 
 // noinspection JSUnresolvedReference
 const dev = process.argv.includes('dev');
@@ -7,19 +7,19 @@ const dev = process.argv.includes('dev');
 // noinspection JSUnresolvedReference
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-	  adapter: adapter(),
-      paths: {
-        base: dev ? '' : process.env.BASE_PATH,
-      }
-	}
+    kit: {
+        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+        // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+        adapter: adapter(),
+        paths: {
+            base: dev ? '' : process.env.BASE_PATH,
+        },
+    }
 };
 
 export default config;
