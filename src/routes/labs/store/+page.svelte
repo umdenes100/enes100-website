1983a227da9847119316d14f7c4fc9a163f1823c
Forrest Milner <forrestmilner+github@gmail.com>
2024-10-10 14:51:24 -0400

    School store instructions

--- src/routes/labs/store/+page.svelte
+++ src/routes/labs/store/+page.svelte
@@ -1,6 +1,5 @@
 <script>
     import MarkdownContent from "$lib/MarkdownContent.svelte";
-    import HeaderBar from "$lib/HeaderBar.svelte";
 </script>
 
 
@@ -9,8 +8,8 @@
         <MarkdownContent>
 # School Store Information
 Welcome to the ENES100 School Store, your one-stop shop for commonly-used OTV hardware! The School Store is open during normal staffed ENES100 hours (i.e., your section’s time and open lab hours) and will be located in the middle of the two labs, JMP 1120 and 1116. The School Store was created to make the OTV project more sustainable while saving you money. The Keystone department has collected and saved hundreds of OTV parts from past ENES100 teams that are now available for you to “purchase”. At the end of the semester, you will return the parts to the School Store for future teams to use.
-## Budget, Available Products, and Pricing 
-Each mission team has a budget of 50 School Store Silver coins. The base stock list and their respective prices can be found below: 
+## Budget, Available Products, and Pricing
+Each mission team has a budget of 50 School Store Silver coins. The base stock list and their respective prices can be found below:
 | Product | Price | Per-team limit |
 | :--- | :--- | :--- |
 | Arduino Uno | $8       | 3 |
@@ -27,12 +26,12 @@ Each mission team has a budget of 50 School Store Silver coins. The base stock l
 
 Since the parts here have been used by previous ENES100 teams, there are no guarantees that School Store Batteries or Arduinos work. After purchasing, try to get soldered/wired as soon as possible to test them. If they are faulty or fried, please return to the School Store to replace them within 5 calendar days. If your team fries an Arduino, you can buy another one, but remember, your team may only buy up to 3 Arduino unos from the School Store for the semester. An arduino that is faulty or fried upon purchasing from the School Store does not count toward the 3 arduino uno and/or 1 arduino mega limit.
 
-Other mission specific and less common parts such as sensors, voltage regulators, and miscellaneous hardware will be available for less than $3, and prices for these items can be checked on the School Store Cart each semester. 
+Other mission specific and less common parts such as sensors, voltage regulators, and miscellaneous hardware will be available for less than $3, and prices for these items can be checked on the School Store Cart each semester.
 
 ## Procedure
-When you would like to purchase a part from the school store, you must do so with a UTF or an LTF on duty. You will choose a team PIN and enter one email address that will be associated with your whole team. Once your PIN is registered, your team will keep using this PIN for all future transactions. To purchase items, the TF will scan each part’s respective barcode, attached to the school store cart, and then check you out. Once you have used your 50 School Store Silver, the system will not allow your team to successfully check out any additional items without returning other items first.
+When you would like to purchase a part from the school store, please talk to a UTF or an LTF on duty. Students may not use the school store application. (check out / purchase).
 
-## For instructors
+## For Instructors
 Click [here](https://enes100.umd.edu/labs/store/checkout) to facilitate sales.
 </MarkdownContent>
     </div>
