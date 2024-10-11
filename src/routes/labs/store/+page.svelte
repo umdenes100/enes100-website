147745e18a9bee871db58b3881d72777dd831bfa
Anuraag <62964880+Onarug@users.noreply.github.com>
2024-10-11 15:28:57 -0400

    Update School Store menu

--- src/routes/labs/store/+page.svelte
+++ src/routes/labs/store/+page.svelte
@@ -8,21 +8,42 @@
         <MarkdownContent>
 # School Store Information
 Welcome to the ENES100 School Store, your one-stop shop for commonly-used OTV hardware! The School Store is open during normal staffed ENES100 hours (i.e., your section’s time and open lab hours) and will be located in the middle of the two labs, JMP 1120 and 1116. The School Store was created to make the OTV project more sustainable while saving you money. The Keystone department has collected and saved hundreds of OTV parts from past ENES100 teams that are now available for you to “purchase”. At the end of the semester, you will return the parts to the School Store for future teams to use.
+## For Instructors
+Click [here](https://enes100.umd.edu/labs/store/checkout) to facilitate sales.
 ## Budget, Available Products, and Pricing
 Each mission team has a budget of 50 School Store Silver coins. The base stock list and their respective prices can be found below:
-| Product | Price | Per-team limit |
-| :--- | :--- | :--- |
-| Arduino Uno | $8       | 3 |
-| Motor Driver | $3 | None |
-| Battery | $15 | 1 |
-| Servos | $3 | None |
-| Motors | $35 for 2 | 2 |
-| Ultrasonic Distance Sensor | $1 | None |
-| Large Wheels and Omni Wheels | $6 each | None |
-| Standard Wheels | $4 each | None |
-| Caster Wheels | $3 each | 2 |
-| Breadboard | $1 | None |
-| Tamiya Connector | $4 | None |
+| Product                     | Price           | Per-team limit |
+| :---------------------------| :-------------- | :------------- |
+| Arduino Uno                  | $8             | 3              |
+| Arduino Mega/Romeo           | $20            | 1              |
+| Breadboard                   | $1             | None           |
+| Motor Hubs                   | $4             | None           |
+| Motor Driver                 | $3             | None           |
+| Ultrasonic Sensor            | $1             | None           |
+| Servos                       | $3             | None           |
+| Motor                        | $23            | None           |
+| Touch Sensor                 | $2             | None           |
+| Transistor                   | $2             | None           |
+| Water Sensor                 | $2             | None           |
+| Flame Sensor                 | $2             | None           |
+| Color Sensor                 | $2             | None           |
+| Pressure Sensor              | $2             | None           |
+| Potentiometer                | $2             | None           |
+| Relays                       | $2             | None           |
+| Limit Switches               | $2             | None           |
+| Voltage Regulator            | $2             | None           |
+| Spacers/Risers               | $2             | None           |
+| Springs/Balloons             | $2             | None           |
+| Tubing/Pipes                 | $2             | None           |
+| Pumps/Fans                   | $2             | None           |
+| Safety Switches              | $3             | None           |
+| Tamiya Connectors            | $4             | None           |
+| Batteries                    | $15            | 1              |
+| Wheels                       | $6             | None           |
+| Small Wheels                 | $4             | None           |
+| Wheels Caster                | $3             | 2              |
+| Omni Wheels                  | $6             | None           |
+
 
 Since the parts here have been used by previous ENES100 teams, there are no guarantees that School Store Batteries or Arduinos work. After purchasing, try to get soldered/wired as soon as possible to test them. If they are faulty or fried, please return to the School Store to replace them within 5 calendar days. If your team fries an Arduino, you can buy another one, but remember, your team may only buy up to 3 Arduino unos from the School Store for the semester. An arduino that is faulty or fried upon purchasing from the School Store does not count toward the 3 arduino uno and/or 1 arduino mega limit.
 
@@ -31,8 +52,7 @@ Other mission specific and less common parts such as sensors, voltage regulators
 ## Procedure
 When you would like to purchase a part from the school store, please talk to a UTF or an LTF on duty. Students may not use the school store application. (check out / purchase).
 
-## For Instructors
-Click [here](https://enes100.umd.edu/labs/store/checkout) to facilitate sales.
+
 </MarkdownContent>
     </div>
 </div>
