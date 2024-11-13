dd4fc62a1766952582a583d1b5163c90f61b71cb
Forrest Milner <forrestmilner+github@gmail.com>
2024-11-12 23:35:14 -0500

    updates to ml

--- src/routes/tools/machinelearning/+page.svelte
+++ src/routes/tools/machinelearning/+page.svelte
@@ -9,8 +9,27 @@
         <div class="text">
         <MarkdownContent>
 # Machine Learning Information and Links
+## <span style="color: red">Machine Learning Update 11/12/2024</span>
+A new version of cameras are available for students. These new cameras are not capable of data collection.
+(They will not create a wifi network wificam-%d). They are more reliable. Please as your UTF to contact Forrest for
+how to swap out your camera. You will recieve a new black camera but not a new green PCB.
+
+### Notes on <span style="color: red">NEW CAMERAS ONLY:</span>
+NEW WifiCams have a new LED blinking system to give you more information about debugging the system.
+
+- Single blip - Setup complete, camera successfully initialized.
+- Triple blip - connected to wifi network / vision system
+
+Error Conditions:
+- Infinite blinking (1s on, 1s off) - This means the CAM is connected to wifi but not connect to vision system. Probably you need to restart the vision system. Make sure the switch is in the right position.
+- Agressive Sawtooth (pulsing) that repeats 2x a second - CAMERA initialization ERROR. This happened every once and a while for me. You must restart, it could also possibly be a broken camera.
+- NO LIGHT ON AT ALL - If you see a single blip and then NO other blips and , this means it was unable to connect to the wifi network. Ensure correct switch position and reset power.
+
+Success Conditions:
+ - Solid light on indicates it has connected to the vision system. Not seeing it on the vision system? Maybe it connected to the other room's vision system. Ensure correct switch position. If this light is solid on, it means that at one point it SUCESSFULLY CONNECTED to *A* vision system for some amount of time.
+
 ## Procedure Summary
-Detailed steps for this process can be found [here](https://docs.google.com/document/d/e/2PACX-1vQ62vc19j6C2-0Kt96RCXP47GEOVDUF8kx1D0DLjhxCqnHA-1scFjtmLmQ8KGbFA4QTX7Vno-3M_Wva/pub).
+Please read the detailed steps for this process [here](https://docs.google.com/document/d/e/2PACX-1vQ62vc19j6C2-0Kt96RCXP47GEOVDUF8kx1D0DLjhxCqnHA-1scFjtmLmQ8KGbFA4QTX7Vno-3M_Wva/pub).
 1. Determine the categories you will classify images in to. (In the lesson, this was thumbs up/thumbs down.)
 2. Didn't do the lesson/activity in class? Feel free to do the activity yourself (it takes about 20 minutes) [here](https://colab.research.google.com/drive/1oN1PaApCx8lXFLCyGs9ZnhK8Sk2WBDNH?usp=sharing).
 3. Gather training images of all categories using a WiFi Cam.
