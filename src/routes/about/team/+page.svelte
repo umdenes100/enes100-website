6994122e381d7faca7c4e6f42a1a080519d8bcbd
cmwharff <115747231+cmwharff@users.noreply.github.com>
2025-01-28 14:53:12 -0500

    updated staff page

--- src/routes/about/team/+page.svelte
+++ src/routes/about/team/+page.svelte
@@ -12,106 +12,73 @@
         <div class="person">
             <img src="/img/testudo.jpg" alt=":(" />
             <div class="bio">
-                Dr. Brian Beaudoin<br />Section(s): 0502<br /><a
-                    href="mailto:beaudoin@umd.edu">beaudoin@umd.edu</a
-                >
+                Brian Beaudoin<br />Section(s): 0401<br />
             </div>
         </div>
         <div class="person">
             <img src="/img/testudo.jpg" alt=":(" />
             <div class="bio">
-                Rick Blanton<br />Section(s): 0501<br /><a
-                    href="mailto:rlbjr@umd.edu">rlbjr@umd.edu</a
-                >
+                Michelle Bensi<br />Section(s): 0101<br />
             </div>
         </div>
         <div class="person">
             <img src="/img/testudo.jpg" alt=":(" />
             <div class="bio">
-                Ethan Burbridge<br />Section(s): FC02<br /><a
-                    href="mailto:etb999@umd.edu">etb999@umd.edu</a
-                >
+                Rick Blanton<br />Section(s): 0602<br />
             </div>
         </div>
         <div class="person">
             <img src="/img/testudo.jpg" alt=":(" />
             <div class="bio">
-                Dr. Kevin Chiu<br />Section(s): 1002<br /><a
-                    href="mailto:knchiu@umd.edu">knchiu@umd.edu</a
-                >
+                Kevin Chiu<br />Section(s): 0601, 0801<br />
             </div>
         </div>
         <div class="person">
             <img src="/img/testudo.jpg" alt=":(" />
             <div class="bio">
-                Aris Cleanthous<br />Section(s): 0301<br /><a
-                    href="mailto:acleanth@umd.edu">acleanth@umd.edu</a
-                >
+                Aris Cleanthous<br />Section(s): 0201<br />
             </div>
         </div>
         <div class="person">
             <img src="/img/testudo.jpg" alt=":(" />
             <div class="bio">
-                Joshua Cocker<br />Section(s): 0302, 0701, FC01<br /><a
-                    href="mailto:jcocker@umd.edu">jcocker@umd.edu</a
-                >
+                Joshua Cocker<br />Section(s): 0701<br />
             </div>
         </div>
         <div class="person">
             <img src="/img/testudo.jpg" alt=":(" />
             <div class="bio">
-                Michael Galczynski<br />Section(s): 0402, 0601, 0702<br /><a
-                    href="mailto:mgalczyn@umd.edu">mgalczyn@umd.edu</a
-                >
+                John Cumings<br />Section(s): 0202<br />
             </div>
         </div>
         <div class="person">
             <img src="/img/testudo.jpg" alt=":(" />
             <div class="bio">
-                Catherine Hamel<br />Section(s): 0401<br /><a
-                    href="mailto:chamel@umd.edu">chamel@umd.edu</a
-                >
+                Catherine Hamel<br />Section(s): 0102, 0302<br />
             </div>
         </div>
         <div class="person">
             <img src="/img/testudo.jpg" alt=":(" />
             <div class="bio">
-                Dr. Amy Karlsson<br />Section(s): 0801<br /><a
-                    href="mailto:ajkarl@umd.edu">ajkarl@umd.edu</a
-                >
+                David Kriesberg<br />Section(s): 0501<br />
             </div>
         </div>
         <div class="person">
             <img src="/img/testudo.jpg" alt=":(" />
             <div class="bio">
-                David Kriesberg<br />Section(s): 0802<br /><a
-                    href="mailto:dkriesbe@umd.edu">dkriesbe@umd.edu</a
-                >
+                Daanish Maqbool<br />Section(s): 1001<br />
             </div>
         </div>
         <div class="person">
             <img src="/img/testudo.jpg" alt=":(" />
             <div class="bio">
-                Dr. Isabel Lloyd<br />Section(s): 0602<br /><a
-                    href="mailto:illoyd@umd.edu">illoyd@umd.edu</a
-                >
+                Alisa Morss Clyne<br />Section(s): 0402<br />
             </div>
         </div>
         <div class="person">
             <img src="/img/testudo.jpg" alt=":(" />
             <div class="bio">
-                Dr. Daanish Maqbool<br />Section(s): 1201<br /><a
-                    href="mailto:daanish.maqbool@gmail.com"
-                    >daanish.maqbool@gmail.com</a
-                >
-            </div>
-        </div>
-        <div class="person">
-            <img src="/img/testudo.jpg" alt=":(" />
-            <div class="bio">
-                Dr. Jarred Young<br />Section(s): 1001<br /><a
-                    href="mailto:jyoung11@umd.edu">jyoung11@umd.edu</a
-                >
+                Peter Sunderland<br />Section(s): 0301<br />
             </div>
         </div>
     </div>
@@ -125,118 +92,99 @@
         <div class="person">
             <img src="/img/testudsnow.jpg" alt=":(" />
             <div class="bio">
-                Ayomide Allen<br />Major: Mechanical Engineering<br />Section:
-                0401
-            </div>
-        </div>
-        <div class="person">
-            <img src="/img/testudsnow.jpg" alt=":(" />
-            <div class="bio">
                 Haroon Amanullah<br />Major: Civil Engineering<br />Section:
-                1002
-            </div>
-        </div>
-        <div class="person">
-            <img src="/img/testudsnow.jpg" alt=":(" />
-            <div class="bio">
-                Samuel Bentz<br />Major: Mechanical Engineering<br />Section:
-                0601
-            </div>
-        </div>
-        <div class="person">
-            <img src="/img/testudsnow.jpg" alt=":(" />
-            <div class="bio">
-                Rita Booth<br />Major: Civil Engineering<br />Section: 0801
-            </div>
-        </div>
-        <div class="person">
-            <img src="/img/testudsnow.jpg" alt=":(" />
-            <div class="bio">
-                Sabine Buccheri<br />Major: Mechanical Engineering<br />Section:
-                1001
-            </div>
-        </div>
-        <div class="person">
-            <img src="/img/testudsnow.jpg" alt=":(" />
-            <div class="bio">
-                Kevin Cheng<br />Major: Mechanical Engineering<br />Section:
-                0302
-            </div>
-        </div>
-        <div class="person">
-            <img src="/img/testudsnow.jpg" alt=":(" />
-            <div class="bio">
-                Will Covington<br />Major: Aerospace Engineering<br />Section:
-                0702
-            </div>
-        </div>
-        <div class="person">
-            <img src="/img/testudsnow.jpg" alt=":(" />
-            <div class="bio">
-                Zoe Edwards<br />Major: Mechanical Engineering<br />Section:
-                0602
-            </div>
-        </div>
-        <div class="person">
-            <img src="/img/testudsnow.jpg" alt=":(" />
-            <div class="bio">
-                Natalie Finkelstein<br />Major: Mechanical Engineering<br
-                />Section: 0701
-            </div>
-        </div>
-        <div class="person">
-            <img src="/img/testudsnow.jpg" alt=":(" />
-            <div class="bio">
-                Anna Fulton<br />Major: Mechanical Engineering<br />Section:
                 0301
             </div>
         </div>
         <div class="person">
             <img src="/img/testudsnow.jpg" alt=":(" />
             <div class="bio">
+                Samuel Bentz<br />Major: Mechanical Engineering<br />Section:
+                0801
+            </div>
+        </div>
+        <div class="person">
+            <img src="/img/testudsnow.jpg" alt=":(" />
+            <div class="bio">
+                Sabine Buccheri<br />Major: Mechanical Engineering<br />Section:
+                0601
+            </div>
+        </div>
+        <div class="person">
+            <img src="/img/testudsnow.jpg" alt=":(" />
+            <div class="bio">
+                Will Covington<br />Major: Aerospace Engineering<br />Section:
+                0401
+            </div>
+        </div>
+        <div class="person">
+            <img src="/img/testudsnow.jpg" alt=":(" />
+            <div class="bio">
+                Zoe Edwards<br />Major: Mechanical Engineering<br />Section:
+                0701
+            </div>
+        </div>
+        <div class="person">
+            <img src="/img/testudsnow.jpg" alt=":(" />
+            <div class="bio">
+                Natalie Finkelstein<br />Major: Mechanical Engineering<br
+                />Section: 0302
+            </div>
+        </div>
+        <div class="person">
+            <img src="/img/testudsnow.jpg" alt=":(" />
+            <div class="bio">
                 Anushka Kaluskar<br />Major: Mechanical Engineering<br
-                />Section: 0402
+                />Section: 0102
             </div>
         </div>
         <div class="person">
             <img src="/img/testudsnow.jpg" alt=":(" />
             <div class="bio">
                 Michael O'Neill<br />Major: Mechanical Engineering<br />Section:
-                0802
+                0501
             </div>
         </div>
         <div class="person">
             <img src="/img/testudsnow.jpg" alt=":(" />
             <div class="bio">
                 Terrence Pierce<br />Major: Mechanical Engineering<br />Section:
-                0502
-            </div>
-        </div>
-        <div class="person">
-            <img src="/img/testudsnow.jpg" alt=":(" />
-            <div class="bio">
-                Hannah Rasmussen<br />Major: Bioengineering<br />Section: 0501
+                0101
             </div>
         </div>
         <div class="person">
             <img src="/img/testudsnow.jpg" alt=":(" />
             <div class="bio">
                 Tyler Rivenbark<br />Major: Aerospace Engineering<br />Section:
-                1201
+                0201
             </div>
         </div>
         <div class="person">
             <img src="/img/testudsnow.jpg" alt=":(" />
             <div class="bio">
                 Stephen Snyder<br />Major: Mechanical Engineering<br />Section:
-                FC01
+                0202
+            </div>
+        </div>
+        <div class="person">
+            <img src="/img/testudsnow.jpg" alt=":(" />
+            <div class="bio">
+                Joshua Stone<br />Major: Computer Engineering<br />Section:
+                1001
             </div>
         </div>
         <div class="person">
             <img src="/img/testudsnow.jpg" alt=":(" />
             <div class="bio">
                 Adithya Sundar<br />Major: Aerospace Engineering<br />Section:
-                FC02
+                0402
+            </div>
+        </div>
+        <div class="person">
+            <img src="/img/testudsnow.jpg" alt=":(" />
+            <div class="bio">
+                Ryan Vacek<br />Major: Fire Protection Engineering<br />Section:
+                0602
             </div>
         </div>
     </div>
@@ -247,12 +195,6 @@
         instructors throughout the semester.
     </h4>
     <div class="split_box">
-        <div class="person">
-            <img src="/img/testudbow.jpg" alt=":(" />
-            <div class="bio">
-                Muallim Cekic<br />Major: Electrical Engineering
-            </div>
-        </div>
         <div class="person">
             <img src="/img/testudbow.jpg" alt=":(" />
             <div class="bio">
@@ -286,12 +228,8 @@
         <div class="person">
             <img src="/img/testudbow.jpg" alt=":(" />
             <div class="bio">
-                Matthew Paul<br />Major: Mechanical Engineering
+                John Paulenich<br />Major: Computer Engineering
             </div>
-        </div>
-        <div class="person">
-            <img src="/img/testudbow.jpg" alt=":(" />
-            <div class="bio">Joshua Stone<br />Major: Computer Engineering</div>
         </div>
         <div class="person">
             <img src="/img/testudbow.jpg" alt=":(" />
@@ -372,10 +310,10 @@
         font-weight: normal;
         font-size: 30px;
     }
-    a {
+    /* a {
         color: white;
     }
     a:hover {
         color: #ffd200;
-    }
+    } */
 </style>
