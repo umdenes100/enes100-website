5754be05fe2602096afeb212397c8e08ad58802a
Forrest Milner <forrestmilner+github@gmail.com>
2025-02-17 18:24:30 -0500

    micropython examples toggle WIP

--- src/routes/documentation/examples/+page.svelte
+++ src/routes/documentation/examples/+page.svelte
@@ -1,18 +1,34 @@
-<script>
-    import MarkdownContent from "$lib/MarkdownContent.svelte";
+<script lang="ts">
+    import ToggleSwitch from "$lib/ToggleSwitch.svelte";
+
+    let language: 'Arduino C++' | 'MicroPython' = 'Arduino C++';
 </script>
 
 <div class="text">
     <h1>Examples</h1>
+    <div style="padding: 10px">
+        <ToggleSwitch bind:value={language} label="Choose a language" design="multi" options={['Arduino C++', 'MicroPython']} fontSize={12}></ToggleSwitch>
+    </div>
     <details>
         <summary>Initializing the Wifi Module and Updating the Location</summary>
-        <div>
-            See the example "SimpleTest.ino" by opening the Arduino editor with the library installed. Then click File->Examples-> Enes100->SimpleTest
-        </div>
+        {#if language === 'MicroPython'}
+            <div>
+                ... todo Ann-Audrey example
+            </div>
+        {:else}
+            <div>
+                See the example "SimpleTest.ino" by opening the Arduino editor with the library installed. Then click File->Examples-> Enes100->SimpleTest
+            </div>
+        {/if}
     </details>
     <details>
         <summary>Setting up an Ultrasonic Sensor</summary>
-        <div>
+        {#if language === 'MicroPython'}
+            <div>
+                ... todo Ann-Audrey example
+            </div>
+        {:else}
+            <div>
             <pre><code>    // defines pins numbers
     const int trigPin = 9;
     const int echoPin = 10;
@@ -40,14 +56,16 @@
         Serial.print&lpar;"Distance: "&rpar;;
         Serial.println&lpar;distance&rpar;;
     &rbrace;</code></pre>
-        </div>
+            </div>
+        {/if}
     </details>
     <details>
         <summary>
             Drive forward until you reach a certain x and y coordinate.
         </summary>
         <div>
-            This code uses the old style of updateLocation and Enes100.location.x. Note that this code is pseudocode. Its purpose is to help you understand how to do certain actions. There are multiple ways to do this.
+            This code uses the old style of updateLocation and Enes100.location.x. Note that this code is pseudocode. Its purpose is to help you understand how to do certain actions. There are
+            multiple ways to do this.
             <pre><code>
     //Setting a target of x = 2.5, y = 1.5
     while &lpar;!Enes100.updateLocation&lpar;&rpar;&rpar;; //will keep running until succesfully update location
@@ -73,12 +91,12 @@
     // This function will make the OTV turn to a certain location.
     void setAngle&lpar;target&rpar; &lbrace;
         print&lpar;"Targeting angle: "&rpar;; println&lpar;target&rpar;;
-        // The following line runs our targeting code WHILE the DIFFERENCE &lpar;subtraction is taking the difference&rpar; is between -thresh and thresh. 
+        // The following line runs our targeting code WHILE the DIFFERENCE &lpar;subtraction is taking the difference&rpar; is between -thresh and thresh.
         // We take the absolute value of the difference in order to compare it to a single threshold.
         while &lpar;abs&lpar;target - currentTheta&rpar; &gt; threshold&rpar; &lbrace;
             turnValue = someConstant * &lpar;target - currentTheta&rpar;;
             turnValue = constrain&lpar;turnValue, -MAX_TURN_SPEED, MAX_TURN_SPEED&rpar;; //Constrain your turn speed.
-            setMotors&lpar;turnValue, -turnValue&rpar;; // You will need to implement this yourself. 
+            setMotors&lpar;turnValue, -turnValue&rpar;; // You will need to implement this yourself.
         &rbrace;
         print&lpar;"Got to angle!"&rpar;;
         setMotors&lpar;0, 0&rpar;;
