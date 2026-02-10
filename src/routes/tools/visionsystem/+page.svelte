a52fdca2474148314399ff1eb8da8e217db0944d
M-Crooks <mcrooks5@terpmail.umd.edu>
2026-02-10 16:43:10 -0500

    updated vision system page with new VS links

--- src/routes/tools/visionsystem/+page.svelte
+++ src/routes/tools/visionsystem/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-    import Construction from "$lib/Construction.svelte";
-import HeaderBar from "$lib/HeaderBar.svelte";
+    // import Construction from "$lib/Construction.svelte";
+    import HeaderBar from "$lib/HeaderBar.svelte";
 </script>
 
 <style>
@@ -13,11 +13,15 @@ import HeaderBar from "$lib/HeaderBar.svelte";
         padding: 20px 30px;
         cursor: pointer;
         text-decoration: underline;
+        font-family: 'Interstate';
+        font-weight: bold;
+        font-style: normal;
+        src: local('Interstate'), url("/img/Interstate-Regular.otf") format('opentype');
     }
 
     .vslink-box {
         display: flex;
-        flex-direction: row;
+        flex-direction: column;
         align-items: center;
         justify-content: center;
         flex-wrap: wrap;
@@ -27,31 +31,37 @@ import HeaderBar from "$lib/HeaderBar.svelte";
         display: flex;
         justify-content: center;
         padding: 20px;
+        color: white;
+    }
+
+    .text {
+        background-color: #121B21;
+        padding: 10px 40px 5%;
+        border: 2px solid white;
+        width: 60%;
+        height: 38%;
+        margin: 40px auto;
     }
 </style>
 
 
 <div class="main_content">
-    <Construction></Construction>
-    <!-- <HeaderBar></HeaderBar>
+    <!-- <Construction></Construction> -->
+    <HeaderBar></HeaderBar>
     <slot>
         <div class="text">
-            The Vision System can be accessed when you connect to the Vision System wifi network. If you are on eduroam the below
-            link will not work. The wifi network is something like "VisionSystem-1120"
+            Click the links below to access the Vision System for your room. Room 1116 is the main classroom with windows, 1120 is the main classroom without windows, and 1201/1215 is the open lab, where the school store is located.
             <div class="vslink-box">
-                <a href="http://lx-enes100-1.eng.umd.edu:8080/" class="vslink">
-                    <button>1120</button>
-                </a>
-                <a href="http://lx-enes100-2.eng.umd.edu:8080/" class="vslink">
+                <a href="http://lx-enes100-3.eng.umd.edu:8080/" class="vslink">
                     <button>1116</button>
                 </a>
-                <a href="http://lx-enes100-3.eng.umd.edu:8080/" class="vslink">
-                    <button>1201</button>
-                </a>
                 <a href="http://lx-enes100-4.eng.umd.edu:8080/" class="vslink">
-                    <button>1215</button>
+                    <button>1120</button>
+                </a>
+                <a href="http://lx-enes100-5.eng.umd.edu:8080/" class="vslink">
+                    <button>1201/1215</button>
                 </a>
             </div>
         </div>
-    </slot> -->
+    </slot>
 </div>
