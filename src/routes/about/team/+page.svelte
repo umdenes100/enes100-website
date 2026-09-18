fbd9e3be98cf301d6ab8d77af50521d96ca8c763
ShreeyaG3 <shreeyagupta103@gmail.com>
2026-09-18 13:48:00 -0400

    added some pictures

--- src/routes/about/team/+page.svelte
+++ src/routes/about/team/+page.svelte
@@ -301,7 +301,7 @@
         <div class="person">
             <img src="/img/testudsnow.jpg" alt=":(" />
             <div class="bio">
-                Sophi Ziler<br />Section:0402
+                Sophi Zeiler<br />Section:0402
             </div>
         </div>
     </div>
